a87620e82c573163643e7c51d23e7c6f77f0bdc3
Ven-Kal <venkalinnovations@gmail.com>
2025-05-12 19:59:28 +0530

    Add files via upload

---
+++ dist/server/utils/multer.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "./cloudinary.js";
+// Define the params object with proper typing
+const storageParams = {
+    folder: "bicycles",
+    allowed_formats: ["jpg", "jpeg", "png"],
+    transformation: [{ width: 800, crop: "limit" }],
+};
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: storageParams,
+});
+export const upload = multer({ storage });
